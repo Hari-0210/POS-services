55afa5103c4f238f885c77b4c8bda84ace51bb40
Hari-0210 <103021640+Hari-0210@users.noreply.github.com>
2022-12-29 10:07:32 +0530

    added

--- app/query/sales.query.js
+++ app/query/sales.query.js
@@ -16,8 +16,13 @@ values
   .join(";")};
 `,
 
-  getSalesQuery: `Select salesMaster.*, customer.name as customerName,CONVERT_TZ(salesMaster.createdTime,'+00:00','+12:30') createdTime 
-  from salesMaster left join customer on customer.customerID = salesMaster.customerID`,
+  getSalesQuery: `Select sm.*, c.name as customerName,CONVERT_TZ(sm.createdTime,'+00:00','+12:30') createdTime,
+  (select JSON_ARRAYAGG(JSON_OBJECT('productID',sp.productID,'productName', p.productName, 'productQty', sp.productQty)) 
+  from salesProducts as sp 
+  INNER JOIN product as p on p.productID = sp.productID
+  where sp.salesMasterID = sm.salesMasterID) as salesProducts
+    from salesMaster as sm left join customer as c on c.customerID = sm.customerID
+  `,
 
   deleteSalesQuery: (
     salesMasterID
