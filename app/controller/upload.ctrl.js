5acddb9887ea35443807481484cb025a1bceecad
Hari-0210 <103021640+Hari-0210@users.noreply.github.com>
2023-01-23 09:05:17 +0530

    added

--- app/controller/upload.ctrl.js
+++ app/controller/upload.ctrl.js
@@ -3,7 +3,7 @@ const { responseMessages } = require("../../utilities/messages");
 
 exports.uploadLogo = async (req, res) => {
   try {
-    let path = "/resources/static/assets/uploads/" + req.file.filename;
+    let path = __basedir + "/resources/static/assets/uploads/" + req.file.filename;
     responseHandler.successResponse(
         res,
         path,
